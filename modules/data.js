b38396389fa43730b4ed32c7b3ed2e1f84d223a0
M-Archer1 <michael-r-archer@outlook.com>
2024-10-19 07:56:33 -0500

    Add data to data.js

---
+++ modules/data.js
@@ -0,0 +1,6 @@
+const scaleLetters = ['C', 'F', 'Bb', 'Eb', 'Ab', 'Db', 'Gb', 'G', 'D', 'A', 'E', 'B'];
+
+const majorMinor = ['Major', 'Minor'];
+
+const sightReading = ['Etude 1', 'Etude 2', 'Etude 3', 'Etude 4', 'Etude 5'];
+
