c8949dd85c1ca6024af749ea5a6f84ca06acd13c
M-Archer1 <michael-r-archer@outlook.com>
2024-10-20 08:23:43 -0500

    Add json file, add code to script.js

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
-import messageData from "./modules/data";
-import { randomIndex } from "./modules/random";
+import messageData from "./modules/data.js";
+import { randomIndex } from "./modules/random.js";
+
+const {scaleLetters, majorMinor, sightReading} = messageData;
+
+console.log(messageData.scaleLetters);
