824e3266b98d43f069cd70a10d90eab62055e11b
M-Archer1 <michael-r-archer@outlook.com>
2024-10-20 07:56:08 -0500

    Create messageData object and export

--- modules/data.js
+++ modules/data.js
@@ -1,6 +1,11 @@
-const scaleLetters = ['C', 'F', 'Bb', 'Eb', 'Ab', 'Db', 'Gb', 'G', 'D', 'A', 'E', 'B'];
+const messageData = {
 
-const majorMinor = ['Major', 'Minor'];
+  scaleLetters: ['C', 'F', 'Bb', 'Eb', 'Ab', 'Db', 'Gb', 'G', 'D', 'A', 'E', 'B'],
 
-const sightReading = ['Etude 1', 'Etude 2', 'Etude 3', 'Etude 4', 'Etude 5'];
+  majorMinor: ['Major', 'Minor'],
+  
+  sightReading: ['Etude 1', 'Etude 2', 'Etude 3', 'Etude 4', 'Etude 5'],
+  
+}
 
+export default messageData;
