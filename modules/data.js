18f725ee3366ae5102dd86b02f0fcc71515280e2
M-Archer1 <michael-r-archer@outlook.com>
2024-10-29 20:19:51 -0500

    Add functionality to data.js

--- modules/data.js
+++ modules/data.js
@@ -1,3 +1,5 @@
+import { randomIndex } from "./random.js";
+
 const messageData = {
 
   scaleLetters: ['C', 'F', 'Bb', 'Eb', 'Ab', 'Db', 'Gb', 'G', 'D', 'A', 'E', 'B'],
@@ -6,6 +8,13 @@ const messageData = {
   
   sightReading: ['Etude 1', 'Etude 2', 'Etude 3', 'Etude 4', 'Etude 5'],
   
+  randomScale() {
+    return this.scaleLetters[randomIndex(this.scaleLetters)] + " " + this.majorMinor[randomIndex(this.majorMinor)];
+  },
+
+  randomEtude() {
+    return this.sightReading[randomIndex(this.sightReading)];
+  },
 }
 
 export default messageData;
